18aabb0218f2dcedcbe3fddc6b1b26cf490e7386
Somesh Ganesh <someshganesh94@gmail.com>
2018-04-04 22:47:40 -0400

    Fixed different onset cell sizes for phase reset

--- MATLAB_files/phaseProcessingNewBin.m
+++ MATLAB_files/phaseProcessingNewBin.m
@@ -20,6 +20,7 @@ for region = 1 : size(same_rephase,1)
     end_idx = same_region_idx_cell{region}(2);
     same_rephase{region} = X(:, start_idx:end_idx).*(exp(1i*phaseX_in(:, start_idx:end_idx)));
     X_complex(:, start_idx:end_idx) = same_rephase{region};
+%     X_complex(:, start_idx:end_idx) = 0;
     
 end
 
@@ -28,26 +29,18 @@ old_rephase = cell(size(old_region_cell));
 
 for region = 1 : size(old_rephase,1)
     
-    old_rephase{region} = pghi(old_region_cell{region}, param.windowSize, param.hopSize, param.windowSize);
+    old_rephase{region} = pghi(0.3.*old_region_cell{region}, param.windowSize, param.hopSize, param.windowSize, 'timeinv');
     start_idx = old_region_idx_cell{region}(1);
     end_idx = old_region_idx_cell{region}(2);
     X_complex(:, start_idx:end_idx) = old_rephase{region};
+%     X_complex(:, start_idx:end_idx) = 0;
     
 end
 
 %% Phase reset for new regions
 
-% new_rephase = cell(size(onsets_new_frames));
-
-% for region = 1 : size(new_rephase, 1)
-%
-%     start_idx = onsets_old_frames{}
-%     new_rephase = phaseX_in(:, );
-%
-% end
-
 new_indices = [];
-for instr = 1 : size(HD_in, 1)
+for instr = 1 : size(new_regions_idx_cell, 1)
     
     new_indices = [new_indices (new_regions_idx_cell{instr}(1):new_regions_idx_cell{instr}(2))];
     
@@ -71,6 +64,7 @@ for frame_no = 1 : size(X,2)
                     if ismember(frame_no, onset_cell{new_onsets})
                        
                         new_onset_loc = find(onset_cell{new_onsets} == frame_no);
+%                         fprintf('%d %d %d %d %d \n', frame_no, bin_no, instr, new_onsets, new_onset_loc);
                         new_frame_no = onsets_old_frames{instr}{new_onsets}(new_onset_loc);
                         spec_value(instr) = WD_in(bin_no, instr) * HD_in(instr, new_frame_no);
                         
@@ -91,7 +85,22 @@ for frame_no = 1 : size(X,2)
                     new_to_old_index = [frame_no,bin_no,spec_max_instr,new_onsets,new_onset_loc];
                     
                     new_frame_no = onsets_old_frames{spec_max_instr}{new_onsets}(new_onset_loc);
-                    new_phase_value(bin_no) = phaseX_in(bin_no, new_frame_no);
+                    
+                    if new_frame_no == 1
+                        if bin_no == 1
+                            new_phase_value(bin_no) = phaseX_in(bin_no, new_frame_no);
+                        else
+                            new_phase_value(bin_no) = new_phase_value(bin_no) + phaseX_in(bin_no, new_frame_no);
+                        end
+                    else
+                        if bin_no == 1
+                            new_phase_value(bin_no) = phaseX_in(bin_no, new_frame_no) - phaseX_in(bin_no, new_frame_no-1);
+                        else
+                            new_phase_value(bin_no) = new_phase_value(bin_no) + phaseX_in(bin_no, new_frame_no) - phaseX_in(bin_no, new_frame_no-1);
+                        end
+                        
+                    end
+                        % Phase continuity 
                     
                 end
                 
@@ -100,6 +109,7 @@ for frame_no = 1 : size(X,2)
         end
         
         X_complex(:, frame_no) = X(:, frame_no).*(exp(1i*new_phase_value));
+%         X_complex(:, frame_no) = 0;
         
     end
     
