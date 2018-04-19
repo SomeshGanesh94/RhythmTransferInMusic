f0d49269e6b5285f754c6b5ff757185d3d99f4b3
Somesh Ganesh <someshganesh94@gmail.com>
2018-04-19 11:50:57 -0400

    Fixed redundant onset processing

--- MATLAB_files/activationProcessing.m
+++ MATLAB_files/activationProcessing.m
@@ -2,7 +2,7 @@
 % Somesh Ganesh
 % MUSI 7100 Fall 2017
 
-function [new_HD_in, onsets_new_frames, onsets_old_frames] = activationProcessing(HD_in, offset_vector_in, input_to_target)
+function [new_HD_in, onsets_new_frames, onsets_old_frames] = activationProcessing(HD_in, offset_vector_in, input_to_target, Tempoffset_vector_in, Tempinput_to_target)
 
 num_instruments = size(HD_in, 1);
 quantization_factor = 32;
@@ -87,29 +87,34 @@ for instr = 1 : num_instruments
 %         if instr == 2 && onset_num == 1
 %             disp(instr);
 %         end
-        
-        if input_to_target{instr}(onset_num) ~= offset_vector_in{instr}(onset_num)
-            
-            final_zone_start = floor((input_to_target{instr}(onset_num) - 1) * size(HD_in, 2) / quantization_factor);
-            start_idx = final_zone_start - floor(blocks / blockd);
-            end_idx = start_idx + blocks - 1;
-            
-            % Checking for condition when start_idx is less than 1
-            if start_idx < 1
-                
-                diff = 1 - start_idx;
-                start_idx = 1;
-                
-                new_HD_in(instr, start_idx : end_idx) = temp_shift_zones(onset_num, (start_idx + diff) : end);
-                
-            else
-                
-                new_HD_in(instr, start_idx : end_idx) = temp_shift_zones(onset_num, :);
-                
+%         if length(Tempoffset_vector_in{instr}) >= onset_num
+%             tempVar = Tempoffset_vector_in{instr};
+%             tempVar(onset_num) = [];
+%         end
+        if ~ismember(input_to_target{instr}(onset_num), offset_vector_in{instr})
+            if input_to_target{instr}(onset_num) ~= offset_vector_in{instr}(onset_num)
+
+                final_zone_start = floor((input_to_target{instr}(onset_num) - 1) * size(HD_in, 2) / quantization_factor);
+                start_idx = final_zone_start - floor(blocks / blockd);
+                end_idx = start_idx + blocks - 1;
+
+                % Checking for condition when start_idx is less than 1
+                if start_idx < 1
+
+                    diff = 1 - start_idx;
+                    start_idx = 1;
+
+                    new_HD_in(instr, start_idx : end_idx) = temp_shift_zones(onset_num, (start_idx + diff) : end);
+
+                else
+
+                    new_HD_in(instr, start_idx : end_idx) = temp_shift_zones(onset_num, :);
+
+                end
+
+                onsets_new_frames{instr}{onset_num} = [start_idx : start_idx + length(onsets_old_frames{instr}{onset_num}) - 1];
+
             end
-            
-            onsets_new_frames{instr}{onset_num} = [start_idx : start_idx + length(onsets_old_frames{instr}{onset_num}) - 1];
-            
         end
         %             plot(new_HD_in(instr,:));
         
